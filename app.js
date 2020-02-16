d23f34bab434f652c5cf0f03386a88e7c78ee1f2
ChrissSav <dai17023@uom.edu.gr>
2020-02-16 18:30:33 +0200

    Add costumers endpoint

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const db =
     host: 'localhost',
     user: 'user',
     password: 'user1234',
-    database: 'fellowtraveller',
+    database: 'hotel_database',
     insecureAuth : true
 });
 
@@ -53,6 +53,81 @@ app.delete("/employee", (req,res) => {
 });
 
 
+
+
+app.get("/employees",async (req,res) => {
+    employees = await GetAllEmployees();
+    if (employees==false){
+        res.send("error");
+    }else{
+        res.send(employees);
+    }
+    
+});
+
+
+function GetAllEmployees(){
+    return new Promise((resolve,reject)=>{
+        sql = "SELECT * FROM employees;"
+        db.query(sql,(err, result) => {
+            if (err){
+                console.log("GetALLEmployees");
+                console.log(err);
+                resolve (false);
+            }
+            else{
+                resolve (result);
+            }
+        })
+    });
+}
+
+
+
+///Costumers
+app.post("/costumer", (req,res) => {
+    res.send("Καταχωρηση πελάτη");
+});
+
+app.put("/costumer", (req,res) => {
+    res.send("Καταχωρηση πελάτη");
+});
+
+app.delete("/costumer", (req,res) => {
+    res.send("Διαγραφη πελάτη");
+});
+
+
+app.get("/costumers",async (req,res) => {
+    employees = await GetAllCostumers();
+    if (employees==false){
+        res.send("error");
+    }else{
+        res.send(employees);
+    }
+    
+});
+
+
+function GetAllCostumers(){
+    return new Promise((resolve,reject)=>{
+        sql = "SELECT * FROM costumers;"
+        db.query(sql,(err, result) => {
+            if (err){
+                console.log("GetAllCostumers");
+                console.log(err);
+                resolve (false);
+            }
+            else{
+                resolve (result);
+            }
+        })
+    });
+}
+
+
+
+
 ///Reservation
 app.post("/reservation", (req,res) => {
     res.send("Καταχωρηση κρατησης");
@@ -65,6 +140,11 @@ app.put("/reservation", (req,res) => {
 app.delete("/reservation", (req,res) => {
     res.send("Διαγραφη κρατησης");
 });
+
+
+
+
+
 
 //Prices
 app.put("/prices", (req,res) => {
