d75b4003796ef1f85fb5a4d378e83ba0750e03b6
ChrissSav <dai17023@uom.edu.gr>
2020-02-16 17:00:16 +0200

    Creation basics endpoints

--- app.js
+++ app.js
@@ -34,3 +34,39 @@ app.get('', (req,res) => {
     res.send("Καλώς ήρθατε στο Api του Hotel Managment System");
 });
 
+
+
+
+
+
+///Empolyees
+app.post("/employee", (req,res) => {
+    res.send("Καταχωρηση χρηστη");
+});
+
+app.put("/employee", (req,res) => {
+    res.send("Καταχωρηση χρηστη");
+});
+
+app.delete("/employee", (req,res) => {
+    res.send("Διαγραφη χρηστη");
+});
+
+
+///Reservation
+app.post("/reservation", (req,res) => {
+    res.send("Καταχωρηση κρατησης");
+});
+
+app.put("/reservation", (req,res) => {
+    res.send("Καταχωρηση κρατησης");
+});
+
+app.delete("/reservation", (req,res) => {
+    res.send("Διαγραφη κρατησης");
+});
+
+//Prices
+app.put("/prices", (req,res) => {
+    res.send("Επεξεργασια τιμών");
+});
